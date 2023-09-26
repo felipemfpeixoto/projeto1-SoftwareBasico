07bd1794edea30826ac1c985a3a43b1e7559dd60
Felipe Peixoto <felipe.m.f.peixoto@gmail.com>
2023-09-26 15:39:12 -0300

    primeiro commit

---
+++ converte.c
@@ -0,0 +1,7 @@
+int utf2varint(FILE *arq_entrada, FILE *arq_saida) {
+    return 0;
+}
+
+int varint2utf(FILE *arq_entrada, FILE *arq_saida) {
+    return 0;
+}
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include converte.h
+
+int main(void) {
+    return 0;
+}
